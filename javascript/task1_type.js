57ebdcf38b2ef8dc9f7f041a929dc8dad39fd45a
Disha Gupta <imdisha4346@gmail.com>
2025-11-09 18:35:12 +0530

    Disha . Add javascript basic practice questions

---
+++ javascript/task1_type.js
@@ -0,0 +1,19 @@
+/* Write 5 variables in the console and use typeof to check their data types. also create one object and one arrray, and check their type using typeof*/
+let name = "Disha";
+let age = 20;
+let isStudent = true;
+let height = 5.2;
+let hobbies = null;
+console.log(name, typeof name); 
+console.log(age, typeof age);
+console.log(isStudent, typeof isStudent);  
+console.log(height, typeof height);
+console.log(hobbies, typeof hobbies);
+let person = {
+    firstName: "Disha",
+    lastName: "Gupta",
+    age: 20
+};
+let colors = ["red", "green", "blue"];
+console.log(person, typeof person); 
+console.log(colors, typeof colors); 
